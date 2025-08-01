8185854f2d084e317756fdcf1ec16d9abf948532
JAO123-ABC <jsilva1999@cs.up.edu.br>
2025-07-31 21:40:50 -0300

    Add files via upload

---
+++ src/components/Saudacao.js
@@ -0,0 +1,15 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function(){
+    return (
+        <Text> Olá Juaum, Seja Bem Vindo!</Text>
+    )
+
+
+}
+
+const Estilo = StyleSheet.create({
+    textoSaudacao:{
+        fontSize:
+    }
+})
